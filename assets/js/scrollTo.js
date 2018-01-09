c68b8dd069ec9bf1075dfec0bd6d97ab0441817c
RWDevelopment <33756545+RWDevelopment@users.noreply.github.com>
2018-01-09 12:55:12 +0100

    Update scrollTo.js

--- assets/js/scrollTo.js
+++ assets/js/scrollTo.js
@@ -1,2 +1,2 @@
-$("#more").click(function() {$("html, body").animate({ scrollTop: $("#to").offset().top - 100}, 600);});
+$("#more").click(function() {$("html, body").animate({ scrollTop: $("#to").offset().top - 100}, 600); return false;});
 $("#brand").click(function () {$("html, body").animate({ scrollTop: 0 }, 600); return false;});
