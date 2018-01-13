4f87b799ccdf72d2fbc4a785e65c66f3be1e9e66
RWDevelopment <33756545+RWDevelopment@users.noreply.github.com>
2018-01-13 18:19:57 +0100

    Update ticker.js

--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 $('#demo_1').t({
    blink:300,
-   caret:'<span style="color:blue;">\u2592</span>',
+   caret:'<span style="color:gray;">\u2592</span>',
    pause_on_click:true
 });
 
